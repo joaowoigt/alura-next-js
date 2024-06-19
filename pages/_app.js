2e53559a8d8b7d175352b12b3df92200125d4cc6
joaowoigt <joaowoigt@gmail.com>
2024-06-19 09:22:19 -0300

    finish project

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({Component, pageProps}) {
         <> 
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true.toString()}/>
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"/>
             
 
